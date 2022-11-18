c9ebc4798bf90f64c6372b5817d8fcb282b1a9f6
Vitor Mendonça <vitorm.braga96@gmail.com>
2022-11-18 18:56:39 -0300

    TiposVariaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Vitor Mendonça";
+       
+        double salarioMinimo = 1300.33;
+
+        short numeroCurto = 1;
+        int   numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; 
+
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+    
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(meuNome);
+        System.out.println(salarioMinimo);
+        System.out.println(numeroCurto);
+        System.out.println(numeroCurto2);
+        System.out.println(VALOR_DE_PI);
+
+    
     }
 }
